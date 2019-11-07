2dbb3ec07c20df0e823221e35a00e0f42a57fedb
Jenipha Rachel <jenipharachel@gmail.com>
2019-11-07 18:33:02 +0530

    listview of movies using FlatList

--- MovieInfoDisplay/App.js
+++ MovieInfoDisplay/App.js
@@ -7,43 +7,88 @@
  */
 
 import React, {Component} from 'react';
-import {StyleSheet, View, Text, Image} from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  FlatList,
+  Image,
+} from 'react-native';
 // import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 // import {faCoffee} from '@fortawesome/free-solid-svg-icons';
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    uri: 'https://upload.wikimedia.org/wikipedia/en/c/c9/Replicas_poster.jpg',
+    title: 'REPLICAS',
+    desc: {
+      time: '1h 50min',
+      genre: 'Thriller/Mystery',
+    },
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    uri: 'https://images-na.ssl-images-amazon.com/images/I/71B6XKhyTaL.jpg',
+    title: 'The Last Song',
+    desc: {
+      time: '1h 45min',
+      genre: 'Tragedy/Romance',
+    },
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    uri: 'https://images-na.ssl-images-amazon.com/images/I/A1c9bOWb6RL.jpg',
+    title: 'The Fault in Our Stars',
+    desc: {
+      time: '1h 30min',
+      genre: 'Tragedy/Romance',
+    },
+  },
+];
+
+function Item({uri, title, desc}) {
+  return (
+    <>
+      <View style={styles.container}>
+        <View style={styles.row}>
+          <View style={styles.col1}>
+            <Image source={{uri}} style={styles.image} />
+          </View>
+          <View style={styles.col2}>
+            <View style={styles.title}>
+              <Text style={{fontSize: 20, fontWeight: 'bold'}}>{title}</Text>
+            </View>
+            <View style={styles.desc}>
+              {/* <FontAwesomeIcon icon={faCoffee} /> */}
+              <Text>{desc.time}</Text>
+              <Text> {desc.genre}</Text>
+            </View>
+            <View style={styles.screen}>
+              <Text style={styles.border}>IMAX</Text>
+              <Text style={styles.border}>4D</Text>
+              <Text style={styles.border}>3D</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+    </>
+  );
+}
 
 export default class App extends Component {
   render() {
     return (
-      <>
-        <View style={styles.container}>
-          <View style={styles.row}>
-            <View style={styles.col1}>
-              <Image
-                source={{
-                  uri:
-                    'https://upload.wikimedia.org/wikipedia/en/c/c9/Replicas_poster.jpg',
-                }}
-                style={styles.image}
-              />
-            </View>
-            <View style={styles.col2}>
-              <View style={styles.title}>
-                <Text style={{fontSize: 20, fontWeight: 'bold'}}>REPLICAS</Text>
-              </View>
-              <View style={styles.desc}>
-                {/* <FontAwesomeIcon icon={faCoffee} /> */}
-                <Text>2h 3min </Text>
-                <Text> Thriller/Mystery</Text>
-              </View>
-              <View style={styles.screen}>
-                <Text style={styles.border}>IMAX</Text>
-                <Text style={styles.border}>4D</Text>
-                <Text style={styles.border}>3D</Text>
-              </View>
-            </View>
-          </View>
-        </View>
-      </>
+      <SafeAreaView style={{flex: 1}}>
+        <FlatList
+          data={DATA}
+          renderItem={({item}) => (
+            <Item title={item.title} desc={item.desc} uri={item.uri} />
+          )}
+          keyExtractor={item => item.id}
+        />
+      </SafeAreaView>
     );
   }
 }
@@ -51,10 +96,12 @@ export default class App extends Component {
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    padding: 20,
+    // justifyContent: 'center',
   },
   row: {
-    flex: 0.3,
+    flex: 0.4,
+    height: 200,
     flexDirection: 'row',
   },
   col1: {
