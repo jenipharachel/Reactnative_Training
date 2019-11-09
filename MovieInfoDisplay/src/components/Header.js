faa65178eefcd3045057a60b1c3ea312e1a01992
Jenipha Rachel <jenipharachel@gmail.com>
2019-11-09 19:46:07 +0530

    commented codes removed

--- MovieInfoDisplay/src/components/Header.js
+++ MovieInfoDisplay/src/components/Header.js
@@ -3,52 +3,6 @@ import {View, Text, StyleSheet, Button, Alert} from 'react-native';
 import ModalSelector from 'react-native-modal-selector';
 
 export default class Header extends Component {
-  // _onSort() {
-  //   // alert('You tapped Sort the button!');
-  //   Alert.alert(
-  //     'Sort Movies',
-  //     'Sort Based On',
-  //     [
-  //       {
-  //         text: 'Cancel',
-  //         onPress: () => console.log('Cancel Pressed'),
-  //         style: 'cancel',
-  //       },
-  //       {
-  //         text: 'Votes',
-  //         onPress: () => {
-  //           this.props.sortByKey('vote_count');
-  //         },
-  //       },
-  //       {text: 'Release Dates', onPress: () => console.log('OK Pressed')},
-  //     ],
-  //     {cancelable: false},
-  //   );
-  // }
-
-  // _onFilter() {
-  //   // alert('You tapped Filter the button!');
-  //   Alert.alert(
-  //     'Filter Movies',
-  //     'Select Language',
-  //     [
-  //       {
-  //         text: 'Cancel',
-  //         onPress: () => console.log('Cancel Pressed'),
-  //         style: 'cancel',
-  //       },
-  //       {
-  //         text: 'en',
-  //         onPress: () => console.log('Ask me later pressed'),
-  //       },
-  //       {text: 'ja', onPress: () => console.log('OK Pressed')},
-  //       {text: 'cn', onPress: () => console.log('OK Pressed')},
-  //       {text: 'ko', onPress: () => console.log('OK Pressed')},
-  //     ],
-  //     {cancelable: false},
-  //   );
-  // }
-
   render() {
     let index = 0;
     const sortData = [
@@ -66,16 +20,6 @@ export default class Header extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.title}>Movie List</Text>
-        {/* <View style={styles.buttonContainer}>
-          <Button style={styles.button} onPress={this._onSort} title="Sort" />
-        </View>
-        <View style={styles.buttonContainer}>
-          <Button
-            style={styles.button}
-            onPress={this._onFilter}
-            title="Filter"
-          />
-        </View> */}
         <View style={{flex: 0.3, justifyContent: 'space-around'}}>
           <ModalSelector
             data={sortData}
@@ -104,12 +48,6 @@ var styles = StyleSheet.create({
     backgroundColor: 'grey',
     flexDirection: 'row',
     justifyContent: 'space-around',
-    alignItems: 'center',
-  },
-  buttonContainer: {
-    flex: 0.3,
-  },
-  button: {
     alignItems: 'center',
   },
   title: {
